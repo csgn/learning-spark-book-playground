214f3b5efc1a872216df20df3a6e5d66536171c1
csgn <dev.csgn@gmail.com>
2024-07-05 17:46:09 +0300

    refactor

--- scripts/run_jobs.sh
+++ scripts/run_jobs.sh
@@ -1,10 +1,30 @@
-SPARK_HOME=/home/${USER}/Playground/learning-spark/spark3
+SPARK_VERSION=3.5.1
+SPARK_NAME=spark-${SPARK_VERSION}-bin-hadoop3
+SPARK_HOME=${PWD}/${SPARK_NAME}
+SPARK_DOWNLOAD_URL=https://dlcdn.apache.org/spark/spark-${SPARK_VERSION}/${SPARK_NAME}.tgz
 SCALA_VERSION=2.12
 PACKAGE_NAME=pg
 PACKAGE_VERSION=1.0
 TARGET_PACKAGE=${PWD}/target/scala-${SCALA_VERSION}/${PACKAGE_NAME}_${SCALA_VERSION}-${PACKAGE_VERSION}.jar
 
+function check_or_download_spark() {
+    if [ ! -d ${SPARK_HOME} ]; then
+        echo "SPARK_HOME is not exist. Downloading..."
+        wget ${SPARK_DOWNLOAD_URL}
+        if [ -f ${SPARK_NAME}.tgz ]; then
+            tar -xvf ${SPARK_NAME}.tgz
+            if [ -d ${SPARK_NAME} ]; then
+                echo "spark-${SPARK_VERSION} downloaded to ${SPARK_HOME} successfully."
+            fi
+        fi
+    else
+        echo "Spark already ensured, proceeding..."
+    fi
+}
+
 function run() {
+    check_or_download_spark
+
     $SPARK_HOME/bin/spark-submit \
         --deploy-mode client \
         --class ${1}.${2} \
