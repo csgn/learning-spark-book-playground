c86299e71c9d29fc75cd258e85bb32fb6960d53c
csgn <dev.csgn@gmail.com>
2024-07-05 17:23:44 +0300

    refactor

--- scripts/run_jobs.sh
+++ scripts/run_jobs.sh
@@ -1,29 +1,20 @@
-SPARK_HOME=/home/csgn/Playground/learning-spark/spark3
+SPARK_HOME=/home/${USER}/Playground/learning-spark/spark3
 SCALA_VERSION=2.12
 PACKAGE_NAME=pg
 PACKAGE_VERSION=1.0
-TARGET_PACKAGE_JAR=${PWD}/target/scala-2.12/pg
+TARGET_PACKAGE=${PWD}/target/scala-${SCALA_VERSION}/${PACKAGE_NAME}_${SCALA_VERSION}-${PACKAGE_VERSION}.jar
 
-function str() {
+function run() {
     $SPARK_HOME/bin/spark-submit \
         --deploy-mode client \
-        --class ch3.Struct \
-        target/scala-${SCALA_VERSION}/${PACKAGE_NAME}_${SCALA_VERSION}-${PACKAGE_VERSION}.jar
+        --class ${1}.${2} \
+        ${TARGET_PACKAGE} 
 }
 
 # Check the argument passed to the script
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 <function_name>"
+    echo "Usage: $0 <package_name> <object_name>"
     exit 1
 fi
 
-# Determine which function to run based on the argument passed
-case "$1" in
-    "str")
-        str
-        ;;
-    *)
-        echo "Function '$1' not recognized."
-        exit 1
-        ;;
-esac
+run $1 $2
